94274ce574bf7d695a9ae731ea2a2b0542b458a9
nikitopich <babchenko.n1908@gmail.com>
2021-03-07 23:44:53 +0300

    Коммит инициализации

---
+++ src/test/java/com/fallindawn/qabottleprod/QaBottleProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallindawn.qabottleprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBottleProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
